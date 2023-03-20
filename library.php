89c4ac5e2f67f95f2b64932f53fd2c90559b9592
apolk25 <apolk25@hanoverstudents.org>
2023-03-20 12:21:21 -0400

    Added basic site layout

---
+++ library.php
@@ -0,0 +1,17 @@
+<?php
+
+function get_database_connection(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "Adampolk25!";
+        $dbname = "helpdesk";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if($conn->connect_error) { 
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }   
+
+?>
