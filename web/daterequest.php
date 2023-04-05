41edcaa9a89cf6a1cae48260524c6d4e30c82bf1
Colejxs <cspencer24@hanoverstudents.org>
2023-04-04 21:47:04 -0400

    part of the thing

--- web/daterequest.php
+++ web/daterequest.php
@@ -14,19 +14,28 @@ include('library.php');
     $date = $conn->real_escape_string($app_date);
 
 
-    $sqlreq =<<<SQL
-    SELECT fields.*
-    FROM fields
-    Left Outer JOIN AFL ON app_id = afl_app_id
-    left outer JOIN Areas ON area_id = afl_areas_id
-    SQL;
-
-    $sql = "select area_name from areas, join applications on app_dates_requested <> $date, ";
-    $result = $conn->query($sqlreq);
-    echo $sql;
+    
     $row = $result->fetch_assoc();
 
 ?>
+
+<script>
+    function getFields(date) {
+        <?php
+            $sqlreq =<<<SQL
+            SELECT Locations.*
+            FROM Locations
+            -- Left outer JOIN applications ON app_id = afl_areas_id
+            Left Outer JOIN AFL ON app_id = afl_app_id
+            Left outer JOIN Areas ON area_id = afl_areas_id  
+            Where app_date <> $date
+            SQL;
+            $result = $conn->query($sqlreq);
+            echo $sql;
+        ?>
+    }
+</script>
+
 
 
 
@@ -35,14 +44,11 @@ include('library.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
 
-
-<h2>Details for Ticket #<?php echo $row['area_name']; ?></h2>
-
 <form method="POST">
-    <input onchange="getFeilds(this.value)" type="date" value=""/>
+    <input id="date" onchange="getFields(this.value)" type="date"/>
 </form>
